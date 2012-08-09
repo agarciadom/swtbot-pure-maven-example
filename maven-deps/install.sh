09049d77b0d407bf3acf22b686c052c912a5391e
Carl-Emil Lagerstedt <carl-emil.lagerstedt@telavox.se>
2012-08-09 14:26:23 +0200

    fix broken install.sh
    install.sh had and incorrect path in it causing it to fail

--- maven-deps/install.sh
+++ maven-deps/install.sh
@@ -25,7 +25,7 @@ install_module() {
     esac
 }
 
-install_module es.uca.swt-3.6.1.pom
+install_module swt/es.uca.swt-3.6.1.pom
 install_module org.eclipse.core.commands-3.6.0.I20100512{.pom,.jar,-sources.jar}
 install_module org.eclipse.equinox.common-3.6.0{.pom,.jar,-sources.jar}
 install_module org.eclipse.jface-3.6.2.M20110210{.pom,.jar,-sources.jar}
